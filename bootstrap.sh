1f2f53d3b584c158aff3e880e647a56229b6def2
ii masamitsu <ii@entap.co.jp>
2021-09-19 05:25:38 +0900

    Add PHPUnit

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,16 @@
 
 set -C
 
+gibo dump Composer Node > .gitignore
+cat <<EOS
+# PHPUnit
+.phpunit.result.cache
+EOS >> .gitignore
+
+composer init -l MIT -s dev
+composer require --dev phpunit/phpunit
+
+npm init -y
 npm install -D prettier @prettier/plugin-php
 
 RESOURCE_URL=https://raw.githubusercontent.com/uguisu-an/php-bootstrap/main/resources
@@ -9,3 +19,10 @@ RESOURCE_URL=https://raw.githubusercontent.com/uguisu-an/php-bootstrap/main/reso
 curl -S "{$RESOURCE_URL}/editorconfig.ini" ./.editorconfig
 curl -S "{$RESOURCE_URL}/prettierrc.json" ./.prettierrc
 curl -S "{$RESOURCE_URL}/prettierignore" ./.prettierignore
+curl -S "{$RESOURCE_URL}/phpunit.xml" ./phpunit.xml
+
+mkdir -p src/Domain/Models
+mkdir -p src/Application/Services
+mkdir -p tests/Unit
+
+curl -S "{$RESOURCE_URL}/tests/TestCase.php" ./tests/TestCase.php
---
+++ resources/tests/TestCase.php
@@ -0,0 +1,8 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase as FrameworkTestCase;
+
+class TestCase extends FrameworkTestCase
+{
+}
