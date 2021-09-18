c8aeb0be1dee65af047156eb99806cfe515636cf
ii masamitsu <ii@entap.co.jp>
2021-09-19 05:11:01 +0900

    add prettierignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+set -C
+
+npm install -D prettier @prettier/plugin-php
+
 RESOURCE_URL=https://raw.githubusercontent.com/uguisu-an/php-bootstrap/main/resources
 
-curl -S "{$RESOURCE_URL}/prettierrc.json" ./.prettierrc
 curl -S "{$RESOURCE_URL}/editorconfig.ini" ./.editorconfig
+curl -S "{$RESOURCE_URL}/prettierrc.json" ./.prettierrc
+curl -S "{$RESOURCE_URL}/prettierignore" ./.prettierignore
