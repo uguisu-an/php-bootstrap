de58892cd65346e0e239229438c11e756cf11af6
ii masamitsu <ii@entap.co.jp>
2021-09-19 05:07:29 +0900

    editorconfigを追加

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # php-bootstrap
+
+## Usage
+
+    curl -s https://raw.githubusercontent.com/uguisu-an/php-bootstrap/main/bootstrap.sh | bash
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 RESOURCE_URL=https://raw.githubusercontent.com/uguisu-an/php-bootstrap/main/resources
 
 curl -S "{$RESOURCE_URL}/prettierrc.json" ./.prettierrc
+curl -S "{$RESOURCE_URL}/editorconfig.ini" ./.editorconfig
