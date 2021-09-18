08fa50d63e8df116e42bd3a7503f1a34c7943bb7
ii masamitsu <ii@entap.co.jp>
2021-09-19 05:34:28 +0900

    fix typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,16 +10,16 @@ npm install -D prettier @prettier/plugin-php
 
 RESOURCE_URL=https://raw.githubusercontent.com/uguisu-an/php-bootstrap/main/resources
 
-curl -s "{$RESOURCE_URL}/editorconfig.ini" > ./.editorconfig
-curl -s "{$RESOURCE_URL}/prettierrc.json" > ./.prettierrc
-curl -s "{$RESOURCE_URL}/prettierignore" > ./.prettierignore
-curl -s "{$RESOURCE_URL}/phpunit.xml" > ./phpunit.xml
+curl -s "${RESOURCE_URL}/editorconfig.ini" > ./.editorconfig
+curl -s "${RESOURCE_URL}/prettierrc.json" > ./.prettierrc
+curl -s "${RESOURCE_URL}/prettierignore" > ./.prettierignore
+curl -s "${RESOURCE_URL}/phpunit.xml" > ./phpunit.xml
 
 gibo dump Composer Node > .gitignore
-curl -s "{$RESOURCE_URL}/gitignore" >> .gitignore
+curl -s "${RESOURCE_URL}/gitignore" >> .gitignore
 
 mkdir -p src/Domain/Models
 mkdir -p src/Application/Services
 mkdir -p tests/Unit
 
-curl -s "{$RESOURCE_URL}/tests/TestCase.php" > ./tests/TestCase.php
+curl -s "${RESOURCE_URL}/tests/TestCase.php" > ./tests/TestCase.php
